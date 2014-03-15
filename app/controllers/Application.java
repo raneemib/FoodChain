9ccfde0fbe732570fd824f6606890d34c736f722
eladyo <eladyona@gmail.com>
2014-03-15 14:12:33 +0200

    up date app

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,20 +7,8 @@ import views.html.*;
 
 public class Application extends Controller {
 
-	  public static void index() {
-        render();
+    public static Result index() {
+        return ok(index.render("Your new application is ready."));
     }
-
-    public static void showUser(String id) {
-         render();
-    }   
-
-    public static void deleteUser(String id) {
-        render();
-    }
-
-    public static void createUser(User user) {
-        render();     
-    } 
 
 }
